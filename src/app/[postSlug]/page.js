26b9b2f77969a75c03bbb5ecbe4308f75ca90865
Facundo Perez Montalvo <facundoperezmontalvo@gmail.com>
2023-10-24 12:16:59 -0300

    update

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -6,7 +6,7 @@ import styles from './postSlug.module.css'
 import { loadBlogPost } from '@/helpers/file-helpers'
 import { MDXRemote } from 'next-mdx-remote/rsc'
 import { BLOG_TITLE } from '@/constants'
-import CodeSnippet from '@/components/CodeSnippet'
+import COMPONENT_MAP from '@/helpers/mdx-components'
 
 export async function generateMetadata({ params }) {
 	const { frontmatter } = await loadBlogPost(params.postSlug)
@@ -25,12 +25,7 @@ async function BlogPost({ params }) {
 				publishedOn={frontmatter.publishedOn}
 			/>
 			<div className={styles.page}>
-				<MDXRemote
-					source={content}
-					components={{
-						pre: CodeSnippet
-					}}
-				/>
+				<MDXRemote source={content} components={COMPONENT_MAP} />
 			</div>
 		</article>
 	)
